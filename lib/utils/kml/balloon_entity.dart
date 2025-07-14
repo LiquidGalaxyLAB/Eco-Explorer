e69ba014e516254ec952d1f0530d50b2019eba6a
Ssdosaofc <147481143+Ssdosaofc@users.noreply.github.com>
2025-07-14 22:54:23 +0530

    feat: Added deforestation timelapse KMLs

--- lib/utils/kml/balloon_entity.dart
+++ lib/utils/kml/balloon_entity.dart
@@ -28,7 +28,7 @@ class BalloonEntity{
 
   static String orbitBalloon(
       Forest forest, String image, double zoom, double tilt, double heading) =>
-  '''
+      '''
   <?xml version="1.0" encoding="UTF-8"?>
   <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
   <Document>
@@ -42,8 +42,45 @@ class BalloonEntity{
           <img src="$image" alt="picture" width="300" height="200" />
           <h2>Latitude: ${forest.lat}</h2>
           <h2>Longitude: ${forest.lon}</h2>
-          <h2>TI4 Area: ${forest.area}</h2>
+          <h2>Area: ${forest.area}</h2>
           <h2>Ecosystem: ${forest.ecosystem}</h2>
+        ]]>
+       </text>
+       <bgColor>ff101816</bgColor>
+     </BalloonStyle>
+   </Style>
+   <Placemark id="ab">
+     <description>
+     </description>
+     <LookAt>
+       <longitude>${forest.lon}</longitude>
+       <latitude>${forest.lat}</latitude>
+       <heading>$heading</heading>
+       <tilt>$tilt</tilt>
+       <range>$zoom</range>
+     </LookAt>
+     <styleUrl>#about_style</styleUrl>
+     <gx:balloonVisibility>1</gx:balloonVisibility>
+     <Point>
+       <coordinates>${forest.lon},${forest.lat},0</coordinates>
+     </Point>
+   </Placemark>
+  </Document>
+  </kml>''';
+
+  static String tourBalloon(
+      Forest forest, String place, double zoom, double tilt, double heading) =>
+      '''
+  <?xml version="1.0" encoding="UTF-8"?>
+  <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+  <Document>
+   <name>About Data</name>
+   <Style id="about_style">
+     <BalloonStyle>
+       <textColor>ffffffff</textColor>
+       <text>
+        <![CDATA[
+          <h1>$place</h1>
         ]]>
        </text>
        <bgColor>ff101816</bgColor>
@@ -165,6 +202,82 @@ class BalloonEntity{
    <gx:balloonVisibility>1</gx:balloonVisibility>
    <Point>
      <coordinates>${fire.lon},${fire.lat},0</coordinates>
+   </Point>
+  </Placemark>
+  </Document>
+  </kml>
+  ''';
+
+  static String speciesBalloon(
+      Species species, Forest forest, String image, double zoom, double tilt, double heading) =>
+      '''
+  <?xml version="1.0" encoding="UTF-8"?>
+  <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+  <Document>
+   <name>About Data</name>
+   <Style id="about_style">
+     <BalloonStyle>
+       <textColor>ffffffff</textColor>
+       <text>
+        <![CDATA[
+          <h1>${species.name}</h1>
+          <img src="$image" alt="picture" width="300" height="200" />
+          <h2>Scientific name: ${species.sci_name}</h2>
+        ]]>
+       </text>
+       <bgColor>ff101816</bgColor>
+     </BalloonStyle>
+   </Style>
+   <Placemark id="ab">
+     <description>
+     </description>
+     <LookAt>
+       <longitude>${forest.lon}</longitude>
+       <latitude>${forest.lat}</latitude>
+       <heading>$heading</heading>
+       <tilt>$tilt</tilt>
+       <range>$zoom</range>
+     </LookAt>
+     <styleUrl>#about_style</styleUrl>
+     <gx:balloonVisibility>1</gx:balloonVisibility>
+     <Point>
+       <coordinates>${forest.lon},${forest.lat},0</coordinates>
+     </Point>
+   </Placemark>
+  </Document>
+  </kml>''';
+
+  static String forestCoverIndicatorBalloon(
+
+      Forest forest, String link, double ratio, double zoom, double tilt, double heading)=>'''
+  <?xml version="1.0" encoding="UTF-8"?>
+  <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+  <Document>
+  <name>AQI Data</name>
+  <Style id="aqi_style">
+   <BalloonStyle>
+     <textColor>ffffffff</textColor>
+     <text>
+      <h1>Deforestation Magnitude</h1>
+      <img src="$link" alt="picture" width="400" height="${400*ratio}" />
+     </text>
+     <bgColor>ff101816</bgColor>
+   </BalloonStyle>
+  </Style>
+  <Placemark id="ab">
+   <description>
+   </description>
+   <LookAt>
+     <longitude>${forest.lon}</longitude>
+     <latitude>${forest.lat}</latitude>
+     <heading>$heading</heading>
+     <tilt>$tilt</tilt>
+     <range>$zoom</range>
+   </LookAt>
+   <styleUrl>#aqi_style</styleUrl>
+   <gx:balloonVisibility>1</gx:balloonVisibility>
+   <Point>
+     <coordinates>${forest.lon},${forest.lat},0</coordinates>
    </Point>
   </Placemark>
   </Document>
